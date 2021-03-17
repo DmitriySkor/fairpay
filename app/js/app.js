2c85c7ce3c561079ac28a088e8867793ef14e908
DmitriySkor <rionskey@gmail.com>
2021-03-17 14:31:32 +0200

    add поправил слайдер - таланты, на главной. Поправил адаптив на странице  - regionalhelden

--- app/js/app.js
+++ app/js/app.js
@@ -70,40 +70,39 @@ for (let el of tabLinks) {
     }
 
     $(".slider_talant").slick({
-      arrows:false,
-      dots: false,
-  infinite: true,
-  speed: 300,
-  slidesToShow: 5,
-  slidesToScroll: 5,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 991,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 5,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
 
 $(".partners_slider").slick({
 arrows:true,
